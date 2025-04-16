5486d46e76865c65a1fd20d8a9897bbb570f9866
MrArun5642 <arunneelam06745@gmail.com>
2025-04-16 11:13:45 +0530

    Added whitenoise for static file handling

--- HR4U/project/settings.py
+++ HR4U/project/settings.py
@@ -150,7 +150,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'HR4U/static')]  # where your raw static files live
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'HR4U/static/css')]  # where your raw static files live
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')         # where collectstatic will collect to
 
 # For production (Render)
